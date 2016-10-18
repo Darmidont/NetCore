8a7868ef977317416492f7814b38cba06f993ca6
Dmytro.Rastsvietalov <Dmytro.Rastsvietalov@infopulse.com>
2016-10-18 20:27:29 +0300

    small changes

---
+++ src/Core1010/Repository/Models/EngagementIndexModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core1010.Repository.Models
+{
+    public class EngagementIndexModel
+    {
+		public IEnumerable<Engagement> Engagements { get; set; }
+    }
+}
